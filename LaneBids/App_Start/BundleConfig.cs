7db01a9af4fe26d3e5f25b1636db2c94c49d7c47
Robert Stanley <robert.stanley@ogsys.com>
2015-04-05 22:03:33 -0500

    Update Admin Maint Page

--- LaneBids/App_Start/BundleConfig.cs
+++ LaneBids/App_Start/BundleConfig.cs
@@ -18,6 +18,9 @@ namespace LaneBids
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular*"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
 
