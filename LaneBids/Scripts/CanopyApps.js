94ccda132f1bba229e84e57bb96753eff9c2317e
Robert Stanley <robertjohnstanley@gmail.com>
2015-08-01 12:38:58 -0500

    got lengths and widths in capopy working

--- LaneBids/Scripts/CanopyApps.js
+++ LaneBids/Scripts/CanopyApps.js
@@ -4,33 +4,61 @@
 
     var CanopyController = function ($scope, $compile, canopyHttpService) {
 
-        $scope.ColumnLengths = { measurements: [{
-            feet: '',
-            inches: ''}]
-        };
-
         var onCanopyError = function (response) {
             $scope.error = "Error getting data.";
         };
 
         //Column Lengths
+        $scope.canopyDetail = {};
+        $scope.canopyDetail.ColumnLengths = {};
+        $scope.canopyDetail.ColumnLengths = {
+            measurements: [{
+                feet: '',
+                inches: ''
+            }]
+        };
         $scope.addColumnLength = function () {
-            $scope.ColumnLengths.measurements.push({});
+            $scope.canopyDetail.ColumnLengths.measurements.push({});
         };
 
         $scope.addSameColumnLenght = function (length) {
 
-            $scope.ColumnLengths.measurements.push({
+            $scope.canopyDetail.ColumnLengths.measurements.push({
                 feet: length.feet,
                 inches: length.inches
             });
         };
 
         $scope.deleteColumnLength = function (index) {
-            $scope.ColumnLengths.measurements.splice(index, 1);
+            $scope.canopyDetail.ColumnLengths.measurements.splice(index, 1);
         };
 
         //Columns Widths
+        $scope.canopyDetail.ColumnWidths = {
+            measurements: [{
+                feet: '',
+                inches: ''
+            }]
+        };
+        $scope.addColumnWidth = function () {
+            $scope.canopyDetail.ColumnWidths.measurements.push({});
+        };
+
+        $scope.addSameColumnWidth = function (width) {
+
+            $scope.canopyDetail.ColumnWidths.measurements.push({
+                feet: width.feet,
+                inches: width.inches
+            });
+        };
+
+        $scope.deleteColumnWidth = function (index) {
+            $scope.canopyDetail.ColumnWidths.measurements.splice(index, 1);
+        };
+
+        $scope.submitForm = function(canopyDetail) {
+            alert(canopyDetail);
+        }
 
         //canopyHttpService.getContacts().then(onContactListComplete, onCanopyError);
         //canopyHttpService.getStateList().then(onStateListComplete, onCanopyError);
