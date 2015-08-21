d00a0370c7e4e97bd6d052fc0f5a2a7848c971c2
Robert Stanley <robertjohnstanley@gmail.com>
2015-08-20 19:43:57 -0500

    working on bid view

--- LaneBids/Models/BidDetailsModel.cs
+++ LaneBids/Models/BidDetailsModel.cs
@@ -15,34 +15,46 @@ namespace LaneBids.Models
 
         [Required(ErrorMessage = "You must choose a Structure Type")]
         public int StructureId { get; set; }
+        public string SelectedStructureName { get; set; }
 
         [Required(ErrorMessage = "You must choose a Bid Type")]
         public int BidTypeId { get; set; }
+        public string SelectedBidTypeName { get; set; }
 
         [Required(ErrorMessage = "You must choose a Bid Status")]
         public int BidStatusId { get; set; }
+        public string SelectedBidStatusName { get; set; }
 
         [Required(ErrorMessage = "You must choose a Job Type")]
         public int JobTypeId { get; set; }
+        public string SelectedJobTypeName { get; set; }
         
         public string JobTypeCode { get; set; }
 
         [Required(ErrorMessage = "You must choose a Scope Type")]
         public int ScopeTypeId { get; set; }
         public string ScopeTypeCode { get; set; }
+        public string SelectedScopeTypeName { get; set; }
 
         [Required(ErrorMessage = "You must choose a Customer")]
         public Guid CustomerId { get; set; }
+        public Customer Customer { get; set; }
 
         [Required(ErrorMessage = "You must choose a Sales Person")]
         public int SalesPersonId { get; set; }
+        public SalesPersonDetailsModel SelectedSalesPerson { get; set; }
 
         [Required(ErrorMessage = "You must choose a Site")]
         public int SiteId { get; set; }
+        public SiteDetailsModel SelectedSite { get; set; }
 
 
         public int ShippingId { get; set; }
+        public AddressModel SelectedShippingAddress { get; set; }
+
         public int BidNotesId { get; set; }
+        public Bid_Notes BidNotes { get; set; }
+
         public string BidNotesText { get; set; }
         public int CanopyId { get; set; }
         public string CreatedBy { get; set; }
